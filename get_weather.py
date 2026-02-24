21b6ecacc432e515c8b5ae4bbad448c932d4d9ab
dickie-tech <njerudicky@gmail.com>
2026-02-24 22:55:06 +0300

    Feat: Created the matplot displaying the trend in Nyeri for past 7days

--- get_weather.py
+++ get_weather.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime,timedelta
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 today = datetime.now()
@@ -25,5 +26,25 @@ df = pd.DataFrame({
       'min_temp': daily_data['temperature_2m_min']
 })
 
+df['date'] = pd.to_datetime(df['date'])
+print(df)
 
+#Create plot
+plt.figure(figsize=(10, 6))
+plt.plot(df['date'], df['max_temp'], marker='o', label='Max Temp')
+plt.plot(df['date'], df['min_temp'], marker='o', label='Min Temp')
+
+# Add labels and title
+plt.xlabel('Date')
+plt.ylabel('Temperature (°C)')
+plt.title('Nyeri Weather - Past 7 Days')
+plt.legend()
+
+# Rotate x-axis labels for readability
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Save the plot
+plt.savefig('weather_chart.png')
+plt.show()
 
